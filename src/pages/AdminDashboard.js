942ddf492a80e40c2e8ca71802209e703fffe131
Nishant sethi <102201027+githubissignedup@users.noreply.github.com>
2023-05-20 16:59:13 +0530

    first commit

---
+++ src/pages/AdminDashboard.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import AdminList from "../components/Admin/AdminList"
+import TheHeader from '../components/TheHeader'
+import AddForm from '../components/Admin/AddForm';
+
+const AdminDashboard = () => {
+    const [openForm, setOpenForm] = useState(false)
+    
+  return (
+    <div>
+         <TheHeader/>
+<AdminList />
+<button className='button-85'
+onClick={() => setOpenForm(true)}>Add New Employee</button>
+
+{
+    openForm && (
+        <AddForm/>
+
+    )
+}
+    </div>
+  )
+}
+
+export default AdminDashboard
